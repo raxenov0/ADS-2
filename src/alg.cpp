b6da880ffcb052035f07518e377096553a1ea975
Аксенов Роман <98100948+raxenov0@users.noreply.github.com>
2022-02-27 16:42:15 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -21,7 +21,7 @@ uint64_t fact(uint16_t n) {
 
 double calcItem(double x, uint16_t n) {
   if (n == 0 || x == 0) return 0;
-  return pown(x,n)/fact(n);
+  return pown(x,n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
